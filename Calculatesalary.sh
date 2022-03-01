1ceae792593fdb2aab7f6af3746970d7c30846cb
pallavi757 <kinekarpallavi21@gmail.com>
2022-03-01 12:48:01 +0530

    calculate salary

---
+++ Calculatesalary.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+isPresent=$((RANDOM%2));
+perHourSalary=20;
+workingHour=0;
+if [ $isPresent -eq 0 ]
+then
+	echo "Employee is absent";
+	workingHour=0;
+elif [ $isPresent -eq 1 ]
+then
+	echo "Employee is present";
+	workingHour=8;
+fi
+salary=$(($perHourSalary * $workingHour));
+echo "Employee has earned $salary $ today";
